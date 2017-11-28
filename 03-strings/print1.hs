b828e585d029f6da4276bd1f42d9e57c1222240e
danielmoi <daniel@ampersandmoi.com>
2017-11-29 07:01:50 +1100

    Move into directory

--- 03-strings/print1.hs
+++ 03-strings/print1.hs
@@ -2,3 +2,14 @@ module Print1 where
 
 main :: IO ()
 main = putStrLn "hello world!"
+
+{-
+ - load this in REPL
+ - :l print1.hs
+ -
+ - run the program
+ - main
+ -
+ - hello world!
+ -}
+
--- 03-strings/print2.hs
+++ 03-strings/print2.hs
@@ -8,3 +8,10 @@ main = do
   putStr ", three, and"
   putStrLn " four!"
 
+
+{-
+*Print2> main
+Count to four for me:
+one, two, three, and four!
+- }
+
